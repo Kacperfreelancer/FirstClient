3d2a13e874777f12c1eebc8254303b86e6ecd1f3
Kacperfreelancer <kacper_kaczmarek03@wp.pl>
2022-06-28 22:19:45 +0200

    added animation menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,24 @@
 const burgerIcon = document.querySelectorAll(".icon");
 const btn = document.querySelector(".burger");
 const menu = document.querySelector(".menu-mobile");
+const elementsMenu = document.querySelectorAll(".menu-item");
 
 const handleBurgerIcon = () => {
   burgerIcon.forEach(bar => {
     bar.classList.toggle("active");
-    menu.classList.toggle("active");
+  });
+  menu.classList.toggle("active");
+
+  navItemAnimation();
+};
+
+const navItemAnimation = () => {
+  let delayTime = 0;
+
+  elementsMenu.forEach(item => {
+    item.classList.toggle("active");
+    item.style.animationDelay = `.${delayTime}s`;
+    delayTime += 2;
   });
 };
 
